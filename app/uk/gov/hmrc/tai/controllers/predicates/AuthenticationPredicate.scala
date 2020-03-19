0c2b54ed42dda015cd295b226618f0da60ec748e
Anand Prakash <anand.prakash@digital.hmrc.gov.uk>
2020-03-19 11:15:21 +0000

    Unauth check for tai-backend

--- app/uk/gov/hmrc/tai/controllers/predicates/AuthenticationPredicate.scala
+++ app/uk/gov/hmrc/tai/controllers/predicates/AuthenticationPredicate.scala
@@ -21,7 +21,7 @@ import play.api.Logger
 import play.api.mvc._
 import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals
 import uk.gov.hmrc.auth.core.retrieve.~
-import uk.gov.hmrc.auth.core.{AuthorisationException, AuthorisedFunctions, ConfidenceLevel}
+import uk.gov.hmrc.auth.core.{AuthorisationException, AuthorisedFunctions, ConfidenceLevel, SessionRecordNotFound}
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.play.bootstrap.controller.BaseController
 
@@ -34,34 +34,8 @@ case class AuthenticatedRequest[A](request: Request[A], nino: Nino) extends Wrap
 class AuthenticationPredicate @Inject()(val authorisedFunctions: AuthorisedFunctions) extends BaseController {
 
   def async(action: AuthenticatedRequest[AnyContent] => Future[Result]): Action[AnyContent] =
-    Action.async { implicit request: Request[AnyContent] =>
-      authorisedFunctions
-        .authorised(ConfidenceLevel.L100)
-        .retrieve(Retrievals.nino and Retrievals.trustedHelper) {
-          case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
-          case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
-          case _                       => throw new RuntimeException("Can't find valid credentials for user")
-        }
-        .recover {
-          case e: AuthorisationException =>
-            Logger.warn("Failed to authorise: " + e.reason)
-            Unauthorized(e.getMessage)
-        }
-    }
+    throw new SessionRecordNotFound
 
   def async[A](bodyParser: BodyParser[A])(action: AuthenticatedRequest[A] => Future[Result]): Action[A] =
-    Action.async(bodyParser) { implicit request =>
-      authorisedFunctions
-        .authorised(ConfidenceLevel.L100)
-        .retrieve(Retrievals.nino and Retrievals.trustedHelper) {
-          case _ ~ Some(trustedHelper) => action(AuthenticatedRequest(request, trustedHelper.principalNino))
-          case Some(nino) ~ _          => action(AuthenticatedRequest(request, Nino(nino)))
-          case _                       => throw new RuntimeException("Can't find valid credentials for user")
-        }
-        .recover {
-          case e: AuthorisationException =>
-            Logger.warn("Failed to authorise: " + e.reason)
-            Unauthorized(e.getMessage)
-        }
-    }
+    throw new SessionRecordNotFound
 }
